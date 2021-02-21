f701c92a3953bc18fa8b63abb17a46cee534a9c7
NiceVivis <1975293119@qq.com>
2021-02-21 18:12:15 +0800

    删除

--- basic/src/main/java/com/vivi/basic/redis/DistributeLock.java
+++ basic/src/main/java/com/vivi/basic/redis/DistributeLock.java
@@ -1,30 +1,30 @@
-package com.vivi.basic.redis;
-
-import com.redis.RedisLock;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Component;
-
-/**
- * @author yangwei
- * @date 2021/1/7 2:40 下午
- */
-@Component
-public class DistributeLock implements InitializingBean {
-
-    @Autowired
-    RedisTemplate redisTemplate;
-
-    private RedisLock redisLock;
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        RedisLock redisLock = new RedisLock();
-        redisLock.setRedisTemplate(redisTemplate);
-        this.redisLock = redisLock;
-    }
-
-    public RedisLock getRedisLock(){
-        return this.redisLock;
-    }
-}
+//package com.vivi.basic.redis;
+//
+//import com.redis.RedisLock;
+//import org.springframework.beans.factory.InitializingBean;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * @author yangwei
+// * @date 2021/1/7 2:40 下午
+// */
+//@Component
+//public class DistributeLock implements InitializingBean {
+//
+//    @Autowired
+//    RedisTemplate redisTemplate;
+//
+//    private RedisLock redisLock;
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        RedisLock redisLock = new RedisLock();
+//        redisLock.setRedisTemplate(redisTemplate);
+//        this.redisLock = redisLock;
+//    }
+//
+//    public RedisLock getRedisLock(){
+//        return this.redisLock;
+//    }
+//}
--- basic/src/main/java/com/vivi/basic/redis/ShareLikeCurrent.java
+++ basic/src/main/java/com/vivi/basic/redis/ShareLikeCurrent.java
@@ -1,108 +1,108 @@
-package com.vivi.basic.redis;
-
-
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-
-/**
- * @author yangwei
- * @date 2021/1/7 2:30 下午
- */
-
-public class ShareLikeCurrent {
-
-    private static final String SHARE_LIKE_ID_KEY = "SHARE_LIKE_ID_KEY_%s";
-    private static final Integer LOCK_EXPIRE_TIME_SECONDS = 10;
-    private static final Integer LOCK_WAIT_TIME_SECONDS = 6;
-
-    private DistributeLock distributeLock;
-
-
-    public Boolean sharelike(ShareLikeDo shareLikeDo){
-        //给用户id加锁
-        String lockey = String.format(SHARE_LIKE_ID_KEY,shareLikeDo.getUserId());
-        String  uuid = UUIDUtil.getID();
-        try {
-            int waitTime = LOCK_WAIT_TIME_SECONDS * 1000;
-
-            //循环调用，为了使改进程在阻塞后能执行,如果是让获取锁的执行逻辑，可以不加循环
-            do{
-                //锁住该用户id
-                if (distributeLock.getRedisLock().lock(lockey,uuid,LOCK_EXPIRE_TIME_SECONDS)){
-
-
-
-                    //获取锁并处理完加锁逻辑后跳出循环
-                    break;
-                }else { //若没有拿到锁，将该进程睡眠
-                    try {
-                        Thread.sleep(100);
-                    }catch (InterruptedException e){
-                        e.printStackTrace();
-                    }
-                }
-
-            }while ((waitTime -= 100) >0 );  //自动解锁
-
-        }catch (Exception e){
-            return false;
-        }finally {
-            distributeLock.getRedisLock().unlock(lockey,uuid); //释放锁
-        }
-        return true;
-    }
-
-    public Boolean sharelikeRedisson(ShareLikeDo shareLikeDo){
-        //给用户id加锁
-        String lockey = String.format(SHARE_LIKE_ID_KEY,shareLikeDo.getUserId());
-        String  uuid = UUIDUtil.getID();
-        try {
-            int waitTime = LOCK_WAIT_TIME_SECONDS * 1000;
-
-            //循环调用，为了使改进程在阻塞后能执行,如果是让获取锁的执行逻辑，可以不加循环
-            do{
-                //锁住该用户id
-                if (distributeLock.getRedisLock().lock(lockey,uuid,LOCK_EXPIRE_TIME_SECONDS)){
-
-
-
-                    //获取锁并处理完加锁逻辑后跳出循环
-                    break;
-                }else { //若没有拿到锁，将该进程睡眠
-                    try {
-                        Thread.sleep(100);
-                    }catch (InterruptedException e){
-                        e.printStackTrace();
-                    }
-                }
-
-            }while ((waitTime -= 100) >0 );  //自动解锁
-
-        }catch (Exception e){
-            return false;
-        }finally {
-            distributeLock.getRedisLock().unlock(lockey,uuid); //释放锁
-        }
-        return true;
-    }
-
-    public void sharelike(){
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(15,20,5, TimeUnit.SECONDS,new ArrayBlockingQueue<>(100),
-                new ThreadPoolExecutor.CallerRunsPolicy());
-
-        try {
-            for (int i = 0;i<=20;i++){
-                ShareLikeDo shareLikeDo = new ShareLikeDo();
-
-                threadPoolExecutor.submit(()->{
-                    sharelike(shareLikeDo);
-                });
-
-            }
-        }catch (Exception e){
-
-        }
-    }
-
-}
+//package com.vivi.basic.redis;
+//
+//
+//import java.util.concurrent.ArrayBlockingQueue;
+//import java.util.concurrent.ThreadPoolExecutor;
+//import java.util.concurrent.TimeUnit;
+//
+///**
+// * @author yangwei
+// * @date 2021/1/7 2:30 下午
+// */
+//
+//public class ShareLikeCurrent {
+//
+//    private static final String SHARE_LIKE_ID_KEY = "SHARE_LIKE_ID_KEY_%s";
+//    private static final Integer LOCK_EXPIRE_TIME_SECONDS = 10;
+//    private static final Integer LOCK_WAIT_TIME_SECONDS = 6;
+//
+//    private DistributeLock distributeLock;
+//
+//
+//    public Boolean sharelike(ShareLikeDo shareLikeDo){
+//        //给用户id加锁
+//        String lockey = String.format(SHARE_LIKE_ID_KEY,shareLikeDo.getUserId());
+//        String  uuid = UUIDUtil.getID();
+//        try {
+//            int waitTime = LOCK_WAIT_TIME_SECONDS * 1000;
+//
+//            //循环调用，为了使改进程在阻塞后能执行,如果是让获取锁的执行逻辑，可以不加循环
+//            do{
+//                //锁住该用户id
+//                if (distributeLock.getRedisLock().lock(lockey,uuid,LOCK_EXPIRE_TIME_SECONDS)){
+//
+//
+//
+//                    //获取锁并处理完加锁逻辑后跳出循环
+//                    break;
+//                }else { //若没有拿到锁，将该进程睡眠
+//                    try {
+//                        Thread.sleep(100);
+//                    }catch (InterruptedException e){
+//                        e.printStackTrace();
+//                    }
+//                }
+//
+//            }while ((waitTime -= 100) >0 );  //自动解锁
+//
+//        }catch (Exception e){
+//            return false;
+//        }finally {
+//            distributeLock.getRedisLock().unlock(lockey,uuid); //释放锁
+//        }
+//        return true;
+//    }
+//
+//    public Boolean sharelikeRedisson(ShareLikeDo shareLikeDo){
+//        //给用户id加锁
+//        String lockey = String.format(SHARE_LIKE_ID_KEY,shareLikeDo.getUserId());
+//        String  uuid = UUIDUtil.getID();
+//        try {
+//            int waitTime = LOCK_WAIT_TIME_SECONDS * 1000;
+//
+//            //循环调用，为了使改进程在阻塞后能执行,如果是让获取锁的执行逻辑，可以不加循环
+//            do{
+//                //锁住该用户id
+//                if (distributeLock.getRedisLock().lock(lockey,uuid,LOCK_EXPIRE_TIME_SECONDS)){
+//
+//
+//
+//                    //获取锁并处理完加锁逻辑后跳出循环
+//                    break;
+//                }else { //若没有拿到锁，将该进程睡眠
+//                    try {
+//                        Thread.sleep(100);
+//                    }catch (InterruptedException e){
+//                        e.printStackTrace();
+//                    }
+//                }
+//
+//            }while ((waitTime -= 100) >0 );  //自动解锁
+//
+//        }catch (Exception e){
+//            return false;
+//        }finally {
+//            distributeLock.getRedisLock().unlock(lockey,uuid); //释放锁
+//        }
+//        return true;
+//    }
+//
+//    public void sharelike(){
+//        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(15,20,5, TimeUnit.SECONDS,new ArrayBlockingQueue<>(100),
+//                new ThreadPoolExecutor.CallerRunsPolicy());
+//
+//        try {
+//            for (int i = 0;i<=20;i++){
+//                ShareLikeDo shareLikeDo = new ShareLikeDo();
+//
+//                threadPoolExecutor.submit(()->{
+//                    sharelike(shareLikeDo);
+//                });
+//
+//            }
+//        }catch (Exception e){
+//
+//        }
+//    }
+//
+//}
